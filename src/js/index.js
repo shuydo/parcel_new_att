a3b9a2e8f8af538cc94fe084b0b84d12c19661fd
Shuydo <shuydo12@gmail.com>
2022-07-29 02:49:18 +0300

    к 6и изо в кейсах добавил 2 и запустил lightbox2

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
-console.log("hello");
+// import $ from 'jquery';
+import $ from 'jquery';
+import 'lightbox2/dist/css/lightbox.min.css';
+// import lightbox from 'lightbox2';
+
+// lightbox.option({
+//   alwaysShowNavOnTouchDevices: true, // albumLabel: "Картинка %1 из %2",
+//   disableScrolling: true, // positionFromTop:0,
+//   fadeDuration: 0, // fitImagesInViewport:true,
+//   imageFadeDuration: 0, // maxWidth:100,
+//   resizeDuration: 0, // maxHeight:200,
+//   wrapAround: true, // showImageNumberLabel:false
+// });
