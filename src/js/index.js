17b3e2ead521f1caedb39c0a6a3c8480a07cf215
Shuydo <shuydo12@gmail.com>
2022-07-29 20:57:59 +0300

    smooth scroll by IDs, var opacity header by scroll

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,28 @@
-// import $ from 'jquery';
 import $ from 'jquery';
 import 'lightbox2/dist/css/lightbox.min.css';
-// import lightbox from 'lightbox2';
+import lightbox from 'lightbox2';
 
-// lightbox.option({
-//   alwaysShowNavOnTouchDevices: true, // albumLabel: "Картинка %1 из %2",
-//   disableScrolling: true, // positionFromTop:0,
-//   fadeDuration: 0, // fitImagesInViewport:true,
-//   imageFadeDuration: 0, // maxWidth:100,
-//   resizeDuration: 0, // maxHeight:200,
-//   wrapAround: true, // showImageNumberLabel:false
-// });
+window.onload = () => {
+  let lastPos = 0;
+  const offset = 80;
+  const header = document.querySelector(".header");
+  const pos = () => window.pageYOffset || document.documentElement.scrollTop;
+  const isHide = () => header.classList.contains("hide");
+
+  window.addEventListener("scroll", () => {
+    if (pos() > lastPos && !isHide() && pos() > offset) {
+      header.classList.add("hide");
+    } else if (pos() < lastPos && isHide()) header.classList.remove("hide");
+    lastPos = pos();
+  });
+};
+
+lightbox.option({
+  alwaysShowNavOnTouchDevices: true, // albumLabel: "Картинка %1 из %2",
+  disableScrolling: true, // positionFromTop:0,
+  fadeDuration: 0, // fitImagesInViewport:true,
+  imageFadeDuration: 0, // maxWidth:100,
+  resizeDuration: 500, // maxHeight:200,
+  wrapAround: true, // showImageNumberLabel:false
+});
+
